b615632f62351bc99419b9f95fc7fd5052943556
Andrew Munoz <ajmunoz411@gmail.com>
2021-08-19 16:28:11 -0700

    favorites obj/array dup logic added

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -9,26 +9,54 @@ class App extends Component {
     this.state = {
       dogs: [],
       favorites: [],
+      // favorites: {},
+      favIds: [],
     }
     this.handleAddtoFavorites = this.handleAddtoFavorites.bind(this);
     this.handleRemovefromFavorites = this.handleRemovefromFavorites.bind(this);
   }
+
   componentDidMount() {
     this.setState({
       dogs: dogs.dogs
     })
     console.log('dogs.dogs', dogs.dogs);
   }
+  // array
+  // handleAddtoFavorites(dog) {
+  //   this.setState({
+  //     favorites: [...this.state.favorites, dog]
+  //   });
+  // };
 
+  // array check for dups
   handleAddtoFavorites(dog) {
-    // e.preventDefault();
-    this.setState({
-      favorites: [...this.state.favorites, dog]
-    });
+    if (!this.state.favIds.includes(dog.id)) {
+      this.setState({
+        favorites: [...this.state.favorites, dog],
+        favIds: [...this.state.favIds, dog.id]
+      });
+    } else {
+      console.error('Dog already added!')
+    }
   };
 
+  // object
+  // handleAddtoFavorites(dog) {
+  //   this.setState((prevState) => {
+  //     console.log('prevState', prevState);
+  //     let favs = Object.assign({}, prevState.favorites);
+  //     if (favs[dog.id] === undefined) {
+  //       favs[dog.id] = dog;
+  //     }
+  //     return {
+  //       favorites: favs
+  //     }
+  //   });
+  // }
+
+  // array
   handleRemovefromFavorites(dog) {
-    // console.log('this', this);
     this.setState({
       favorites: this.state.favorites.filter(favDog => (
         favDog.id !== dog.id
@@ -36,11 +64,22 @@ class App extends Component {
     });
   };
 
+  // object
+  // handleRemovefromFavorites(dog) {
+  //   this.setState((prevState) => {
+  //     let favs = Object.assign({}, prevState.favorites);
+  //     delete favs[dog.id];
+  //     return {
+  //       favorites: favs
+  //     }
+  //   });
+  // }
+
   render() {
     return(
       <div>
         <h1 className="title">Dogs of the World</h1>
-        <Favorites favs={this.state.favorites} handleRemovefromFavorites={this.handleRemovefromFavorites}/>
+        <Favorites favs={Object.values(this.state.favorites)} handleRemovefromFavorites={this.handleRemovefromFavorites}/>
         <DogList dogs={this.state.dogs} handleAddtoFavorites={this.handleAddtoFavorites}/>
       </div>
     )
